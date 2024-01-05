a42a903a643e8a3a4f261000629c21a6fbe0a58a
regan-mu <regansomi@gmail.com>
2024-01-05 23:38:54 +0300

    Connect Postgresql database

--- API/config.py
+++ API/config.py
@@ -3,7 +3,7 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET')
-    SQLALCHEMY_DATABASE_URI = "sqlite:///app.db"  # os.environ.get('SQLALCHEMY_DATABASE_URI')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('KINYOZI_DB')  # "sqlite:///app.db"
     MAIL_SERVER = 'smtp.googlemail.com'
     MAIL_PORT = 587
     MAIL_USE_TLS = True
